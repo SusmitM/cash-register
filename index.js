0825144e5a8690e4f9872db32f0ce0a806913a5e
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2022-01-23 20:06:42 +0530

    js done

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
-const checkBtn= document.querySelector("#btnCheck");
-const errorMessage=document.querySelector("#error-message")
-checkBtn.addEventListener("click", function check(){
-    if(billAmount.value>0){
-   if (cashGiven.value>0){
-       if(cashGiven.value>= billAmount.value){
+const checkBtn = document.querySelector("#btnCheck");
+const errorMessage = document.querySelector("#error-message");
+const quantityOfNotes = document.querySelectorAll(".no-of-notes");
+const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
+checkBtn.addEventListener("click", validateBillAmount);
+ 
+function validateBillAmount() {
+    
+    if (billAmount.value > 0) {
+
+        if (cashGiven.value >= billAmount.value) {
+
+            const balanceAmount = cashGiven.value - billAmount.value;
+            calculateChange(balanceAmount);
+        } 
         
-           const balanceAmount = cashGiven.value - billAmount.value;
-           calculateChange(balanceAmount);
-       }
-       else{
-           showMessage("Cash given should be greater than the bill");
-       }
-   }
-   else{
-       showMessage("Cash given should be greater than 0");
-   }
-}
-else{
-    showMessage("Bill Amount should be greater than 0");
+    } 
+    else {
+        showMessage("Invalid Bill Amount");
 
-}
-});
-function calculateChange(balanceAmount){
-
-};
-function showMessage(msg){
-    errorMessage.innerText= msg;
+    }
 }
 
+function calculateChange(balanceAmount) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        const noteQuantity = Math.trunc(balanceAmount / availableNotes[i]);
+        balanceAmount = balanceAmount % availableNotes[i];
+        quantityOfNotes[i].innerText = noteQuantity;
+    }
 
+}
+
+function showMessage(msg) {
+    errorMessage.innerText = msg;
+}
