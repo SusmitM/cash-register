e183cb44c250a255982e77a632aa30d4b82022ed
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2022-01-23 15:07:26 +0530

    fix: JS file

--- index.js
+++ index.js
@@ -2,10 +2,32 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkBtn= document.querySelector("#btnCheck");
 const errorMessage=document.querySelector("#error-message")
-
 checkBtn.addEventListener("click", function check(){
-    console.log(billAmount.value);
-    console.log(cashGiven.value);
+    if(billAmount.value>0){
+   if (cashGiven.value>0){
+       if(cashGiven.value>= billAmount.value){
+        
+           const balanceAmount = cashGiven.value - billAmount.value;
+           calculateChange(balanceAmount);
+       }
+       else{
+           showMessage("Cash given should be greater than the bill");
+       }
+   }
+   else{
+       showMessage("Cash given should be greater than 0");
+   }
+}
+else{
+    showMessage("Bill Amount should be greater than 0");
+
+}
 });
+function calculateChange(balanceAmount){
+
+};
+function showMessage(msg){
+    errorMessage.innerText= msg;
+}
 
 
